3eee9eaaa637e6f4c4c953058ec562d00af7f594
Kevin Terry <kevin.terry@commercetools.com>
2025-01-23 18:00:23 -0500

    added basic user schema and config for codegen

---
+++ backend/codegen.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "./src/schema.graphql",
+  generates: {
+    "./src/generated-types.ts": {
+      plugins: ["typescript", "typescript-resolvers"],
+      //   config: {
+      //     // contextType: #TODO#
+      //   },
+    },
+  },
+};
+
+export default config;
